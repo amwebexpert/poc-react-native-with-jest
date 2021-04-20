aec2c9de0aeeada0caeb58b13345a6f9858aeb70
Andre Masson <amwebexpert@gmail.com>
2021-04-20 14:07:43 -0400

    Added 2 snapshot flavors (Jest vs RTL)

--- __tests__/App-test.tsx
+++ __tests__/App-test.tsx
@@ -2,14 +2,32 @@ import React from 'react';
 import { render } from '@testing-library/react-native';
 import App from '../App';
 
-it('renders correctly', async () => {
+import renderer from 'react-test-renderer';
+
+it('renders correctly A', () => {
+    // when
+    const tree = renderer.create(<App />).toJSON();
+
+    // then
+    expect(tree).toMatchSnapshot();
+});
+
+it('renders correctly B', () => {
+    // when
+    const { toJSON } = render(<App />);
+
+    // then
+    expect(toJSON()).toMatchSnapshot();
+});
+
+it('Display welcome message', async () => {
     // given
-    const { getByText, toJSON } = render(<App />);
+    const { getByText } = render(<App />);
 
     // when
     const welcome = getByText("Welcome to unit tested app!");
 
     // then
     expect(welcome).toBeDefined();
-    expect(toJSON()).toMatchSnapshot();
 });
+
